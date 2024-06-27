80d944c4c033c44c12256660429b2ca9b9d49e08
tedi005 <tedidervishi@gmail.com>
2024-06-27 23:55:37 +0200

    clean code

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index2, name="index"),
-    path('delete/<str:city>/', views.delete_item, name='delete_item')
+    path("", views.index, name="index"),
+    path('delete/<str:city>/', views.delete_item, name='delete')
 ]
