9645c1edafcbeb465bf1276019b2415c5c3c06d6
tedi005 <tedidervishi@gmail.com>
2025-02-07 23:08:11 +0100

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('delete/<str:city>/', views.delete_item, name='delete'),
+]
+
